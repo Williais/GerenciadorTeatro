d0499f93280206c0f9281a961ec1949191340ba1
Willian Padilha <45665895+Williais@users.noreply.github.com>
2026-05-03 20:02:53 -0300

    Initial commit

---
+++ src/br/edu/ifpb/teatro/enums/PessoaSexo.java
@@ -0,0 +1,7 @@
+package br.edu.ifpb.teatro.enums;
+
+public enum PessoaSexo {
+    MASCULINO,
+    FEMININO,
+    OUTRO
+}
---
+++ src/br/edu/ifpb/teatro/model/Pessoa.java
@@ -0,0 +1,45 @@
+package br.edu.ifpb.teatro.model;
+
+import br.edu.ifpb.teatro.enums.PessoaSexo;
+
+public class Pessoa {
+    private String nome;
+    private PessoaSexo sexo;
+    private String cpf;
+    private String email;
+
+    public Pessoa(String email, String cpf, PessoaSexo sexo, String nome) {
+        this.email = email;
+        this.cpf = cpf;
+        this.sexo = sexo;
+        this.nome = nome;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public PessoaSexo getSexo() {
+        return sexo;
+    }
+
+    public void setSexo(PessoaSexo sexo) {
+        this.sexo = sexo;
+    }
+
+    public String getCpf() {
+        return cpf;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+}
